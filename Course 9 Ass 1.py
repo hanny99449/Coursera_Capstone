490df0f840232b4d0c3a1939a3ec4d223f44705a
hanny99449 <63201691+hanny99449@users.noreply.github.com>
2020-04-06 02:58:04 +0800

    Add files via upload

---
+++ Course 9 Ass 1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
